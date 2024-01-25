9e2dd690c116a3d8a79c563ded1d849da8e6d0de
Pranav krishna U <97517105+amenu4aie2@users.noreply.github.com>
2024-01-25 22:51:56 +0530

    Time: 62 ms (69.23%), Space: 17.8 MB (58.68%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,22 +1,9 @@
 class Solution:
 
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        y=sorted(nums)
-        out=[]
-        l,r=0,len(nums)-1
-        while l<r:
-            sum=y[l]+y[r]
-            if(sum==target):
-                out.append(nums.index(y[l]))
-                out.append(len(nums)-1-(nums[::-1].index(y[r])))
-                # SPACE COMPLEXITY OF ABOVE LINE IS O(N)
-                # TIME COMPLEXITY OF ABOVE LINE IS O(N)(FOR INEX OPERATION) +O(N) (FOR REVERSED COPY CREATION)
-                l+=1
-                r-=1
-                
-            elif(sum<target):
-                l+=1
-            else:
-                r-=1
-        
-        return out
+        n = {i:j for j,i in enumerate(nums)}
+        for k,i in enumerate(nums):
+            if target-i in n:
+                if k==n[target-i]:continue
+                return [k,n[target-i]]
+        return [0]
