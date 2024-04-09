37258ab7fdaf4a58fd33af053324a4b4d1b0ad8d
Pranav krishna U <97517105+amenu4aie2@users.noreply.github.com>
2024-04-09 14:01:03 +0530

    Time: 4906 ms (5.11%), Space: 17.2 MB (98.37%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -1,18 +1,12 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        # initialize pointers to the beginning and end of the array
-        left = 0
-        right = len(numbers) - 1
+        x=[]
+        for i in range(len(numbers)):
+            # if(target>=numbers[i]):
+                if((target-numbers[i]) in numbers):
+                    x.append(i+1)
+                    x.append(len(numbers[:i+1])+numbers[i+1:].index(target-numbers[i])+1)
+                    break
         
-        # move the pointers inwards until the sum of the numbers at the pointers equals the target
-        while left < right:
-            sum = numbers[left] + numbers[right]
-            if sum == target:
-                return [left+1, right+1]
-            elif sum < target:
-                left += 1
-            else:
-                right -= 1
-        
-        # if no such pair of numbers exists, return an empty list
-        return []
+
+        return x
