13298fd11d7bc8834c5820a316b56710e709a36b
Pranav krishna U <97517105+amenu4aie2@users.noreply.github.com>
2024-07-20 10:43:23 +0530

    Time: 67 ms (44.09%), Space: 18.3 MB (7.22%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,12 +1,20 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        num_massive = {}
+        x=[]
+        for i,j in enumerate(nums):
+            x.append([i,j])
+        x.sort(key=lambda x:x[1])
+        print(x)
+        
+        f,lp=0,len(nums)-1
+        while f<lp:
+            if(x[f][1]+x[lp][1]==target):
+                return [x[f][0],x[lp][0]]
+            elif(x[f][1]+x[lp][1]<target):
 
-        for i, num in enumerate(nums):
-            subtracter = target - num
+                f+=1
+            elif(x[f][1]+x[lp][1]>target):
 
-            if subtracter in num_massive:
-                return [num_massive[subtracter], i]
-
-            num_massive[num] = i
-        return []
+                lp-=1
+            
+        
