4e67d75efed5c2097616b43fe4b968eef2e1d995
Pranav krishna U <97517105+amenu4aie2@users.noreply.github.com>
2024-07-20 12:56:49 +0530

    Time: 3729 ms (5.25%), Space: 656.9 MB (5.01%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.py
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.py
@@ -1,32 +1,36 @@
-
 class Solution:
-    def mincostTickets(self, days: List[int], costs: List[int]) -> int:
+    def mincostTickets(self, days: List[int], co: List[int]) -> int:
         arr=[0]*(days[-1])
+        dp={}
         for i in days:
-            arr[i-1] = 1
-        mc =float('inf')
-        pd = [1,7,30]
-        pdc = list(zip(pd,costs))
-        d = set()
-        def bt(i,c):
-            d.add((i,c))
-            nonlocal mc
-            if i>=len(arr):
-                mc = min(mc,c)
+            arr[i-1]=1
+        mincost=[float('inf')]
+        def backtrack(i,c):
+            # goal constraints
+            
+            if((i,c) in dp):
                 return
-            for j,k in pdc:
-                
-                if any(arr[i:i+j]): 
-                    if (i+j,c+k) in d:
-                        continue
-                    bt(i+j,c+k)
-                else:
-                    if (i+j,c) in d:
-                        continue
-                    bt(i+j,c)
-            return
-        bt(0,0)
-        return mc if mc!=float('inf') else -1
-            
-            
+            dp[(i,c)]=0
+            if(i>=days[-1]):
+                mincost[0]=min(mincost[0],c)
+
+                return 
+            if(c>mincost[0]):
+                return
+            if(any(arr[i:i+1])):
+
+                backtrack(i+1,c+co[0])
+            else:
+                backtrack(i+1,c)
+            if(any(arr[i:i+7])):
+                backtrack(i+7,c+co[1])
+            else:
+                backtrack(i+7,c)
+            if(any(arr[i:i+30])):
+                backtrack(i+30,c+co[2])
+            else:
+                backtrack(i+30,c)
+        backtrack(0,0)
+        return mincost[0]
+
             
