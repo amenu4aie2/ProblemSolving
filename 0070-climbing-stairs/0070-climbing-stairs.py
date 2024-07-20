533dd9729af84fca5abef926ebfdb5fb8724755b
Pranav krishna U <97517105+amenu4aie2@users.noreply.github.com>
2024-07-20 13:20:28 +0530

    Time: 34 ms (60.38%), Space: 16.7 MB (13.37%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,11 +1,16 @@
-from itertools import permutations
 class Solution:
-   
     def climbStairs(self, n: int) -> int:
-        
-        one,two=1,1
-        for i in range(n-1):
-            temp=one
-            one=one+two
-            two=temp
-        return one
+        dp={}
+        def backtrack(i,t):
+            if(i>=n):
+                return 1
+            if((i,t) in dp):
+                return dp[(i,t)]
+            
+            dp[(i,t)]=backtrack(i+1,t+1)+backtrack(i+2,t+2)
+            return dp[(i,t)]
+
+        return backtrack(1,0)
+
+
+            
