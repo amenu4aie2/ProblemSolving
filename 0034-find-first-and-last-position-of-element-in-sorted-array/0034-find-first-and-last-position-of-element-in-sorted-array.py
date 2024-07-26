477f5bfab263528bc0577486190fe084500383f5
Pranav krishna U <97517105+amenu4aie2@users.noreply.github.com>
2024-07-26 05:31:22 +0530

    Time: 79 ms (34.65%), Space: 17.9 MB (25.59%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,27 +1,24 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        l=0
-        r=len(nums)-1
-        mid=(l+r)//2
-        while l<=r:
-            
-            if(nums[mid]==target):
-                i=mid
-                j=mid
-                
-                while((i)!=len(nums)-1 and nums[(i+1)]==target):
-                    i+=1
-                
+        return [self.binarSearch(nums,0,target),self.binarSearch(nums,1,target)]
 
-                while(j!=0 and nums[j-1]==target):
-                    j-=1
-                    print(j)
-                return [j,i]
-
-                
-            elif(nums[mid]<target):
-                l=mid+1
-            elif(nums[mid]>target):
-                r=mid-1
+    def binarSearch(self,n,b,t):
+        l,r=0,len(n)-1
+        i=-1
+        while(l<=r):
             mid=(l+r)//2
-        return [-1,-1]
+            if(n[mid]<t):
+                l=mid+1
+
+
+
+            elif(n[mid]>t):
+                r=mid-1
+            else:
+                i=mid
+                if b:
+                    l=mid+1
+                else:
+                    r=mid-1
+        
+        return i
