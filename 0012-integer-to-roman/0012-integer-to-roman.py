584b1cec0df99864f5788c2567219868c3d82005
Pranav krishna U <97517105+amenu4aie2@users.noreply.github.com>
2024-08-23 05:14:53 +0530

    Time: 50 ms (40.41%), Space: 16.5 MB (53.44%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.py
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -1,26 +1,26 @@
 class Solution:
-    def __init__(self):
-        self.dict={1:"I",5:"V",10:"X",50:"L",100:"C",500:"D",1000:"M",4:"IV",9:"IX",40:"XL",90:"XC",400:"CD",900:"CM",1000:"M"}
-        self.arr=[1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
-    def intToRoman(self, num: int,i=0) -> str:
-        
-        if(num==0):
-            return ""
-        elif(num>=1000):
-            a=""
-            for i in range((num//1000)):
-                a+="M"
-            return a+self.intToRoman(num%1000)
-        else:
-            a=""
-            while i<=11:
+    def intToRoman(self, num: int) -> str:
+        roman_numerals = {
+            1: 'I',
+            4: 'IV',
+            5: 'V',
+            9: 'IX',
+            10: 'X',
+            40: 'XL',
+            50: 'L',
+            90: 'XC',
+            100: 'C',
+            400: 'CD',
+            500: 'D',
+            900: 'CM',
+            1000: 'M'
+        }
 
-                if(num<=self.arr[i] and num>=self.arr[i+1]):
-                    
-                    for k in range((num//self.arr[i+1])):
-                        a+=self.dict[self.arr[i+1]]
-                    num=(num%self.arr[i+1])
-                    break
-                i+=1
 
-            return a+self.intToRoman(num,i)
+        s=""
+        for i in sorted(roman_numerals.keys(),reverse=True):
+            if(num>=i):
+                s+=roman_numerals[i]*(num//i)
+                num=num%i
+
+        return s
