dde7e28902027bedd0fce7fd5b3370027b28d8f5
Pranav krishna U <97517105+amenu4aie2@users.noreply.github.com>
2024-11-09 10:09:28 +0530

    Time: 0 ms (100%), Space: 16.8 MB (6.93%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -1,14 +1,16 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        t=[[1],[1,1],[1,2,1]]
-        if(numRows==1 or numRows==2 or numRows==2):
-            return t[:numRows]
+        y=[[1],[1,1]]
+        if(numRows==1):
+            return [[1]]
+            
+        elif(numRows==2):
+            return y
+        else:
+            for i in range(2,numRows):
+                y.append([1])
+                for i in range(len(y[-2])-1):
+                    y[-1].append(y[-2][i]+y[-2][i+1])
+                y[-1].append(1)
 
-        for i in range(2,numRows-1):
-            x=len(t[-1])
-            t.append([1])
-            for j in range(x-1):
-                t[-1].append(t[-2][j]+t[-2][j+1])
-            t[-1].append(1)
-        print(t)
-        return t
+        return y
