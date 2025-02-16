787f587af668cdc6019b4185d027bef2ccffae4c
Pranav krishna U <97517105+amenu4aie2@users.noreply.github.com>
2025-02-16 16:57:55 +0530

    Time: 56 ms (23.54%), Space: 48.6 MB (67.66%) - LeetHub

---
+++ 2667-create-hello-world-function/2667-create-hello-world-function.js
@@ -0,0 +1,14 @@
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    return function(...args) {
+        return "Hello World";
+    }
+};
+createHelloWorld()();
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
